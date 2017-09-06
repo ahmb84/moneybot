e095fbf4095aefea6c8a4c8d08b0e2810175d7f3
Adam Rothman <rothman.adam@gmail.com>
2017-09-05 22:29:35 -0700

    Log simulated trades during backtest

--- moneybot/market/adapters/backtest.py
+++ moneybot/market/adapters/backtest.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
+from logging import getLogger
 from typing import Dict
 
 from moneybot.market.adapters import MarketAdapter
 from moneybot.strategy import ProposedTrade
+
+
+logger = getLogger(__name__)
 
 
 class BacktestMarketAdapter(MarketAdapter):
@@ -14,5 +18,6 @@ class BacktestMarketAdapter(MarketAdapter):
         self,
         proposed_trade: ProposedTrade,
     ) -> Dict[str, float]:
+        logger.debug(f'Simulating trade: {proposed_trade}')
         balances = self.market_state.simulate_trades([proposed_trade])
         return balances
