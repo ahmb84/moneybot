620a90c692fa7a944b82e05488c164b79dac7bfb
Adam Rothman <rothman.adam@gmail.com>
2017-09-06 09:28:38 -0700

    Bump pyloniex to 0.0.4 to fix rate limiting; remove Dockerfile

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
         'numpy',
         'pandas',
         'psycopg2',
-        'pyloniex>=0.0.3',
+        'pyloniex>=0.0.4',
         'PyStaticConfiguration[yaml]',
         'requests',
     ],
