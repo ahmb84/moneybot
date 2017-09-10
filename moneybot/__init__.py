db19d58634cb405b16001d9ca8d6332d253b2f33
Adam Rothman <rothman.adam@gmail.com>
2017-09-10 14:47:28 -0700

    Add timestamp to log lines (closes #49 for realsies)

--- moneybot/__init__.py
+++ moneybot/__init__.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
+import logging
+
 import staticconf
 
 
 CONFIG_NS = 'moneybot'
 
+
+logging.basicConfig(
+    format='[%(asctime)s %(levelname)s %(name)s] %(message)s',
+)
 config = staticconf.NamespaceReaders(CONFIG_NS)
 
 
